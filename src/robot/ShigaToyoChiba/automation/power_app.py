6d924151b2d5aff01e6c6fd32922e65dbffdc11c
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2025-12-16 08:54:30 +0700

    3Xuong: wait for uploading

--- src/robot/ShigaToyoChiba/automation/power_app.py
+++ src/robot/ShigaToyoChiba/automation/power_app.py
@@ -1,4 +1,5 @@
 import time
+
 from playwright._impl._errors import TimeoutError
 from playwright.sync_api import Browser, BrowserContext, Playwright
 
@@ -11,7 +12,7 @@ class PowerApp:
         playwright: Playwright,
         browser: Browser,
         context: BrowserContext,
-        domain: str = "https://apps.powerapps.com/play/e/default-3255306b-4eff-41c9-89fb-3e24e65f48a1/a/f1e99b17-afcd-4d42-9af3-c4f9ee79469d?tenantId=3255306b-4eff-41c9-89fb-3e24e65f48a1"
+        domain: str = "https://apps.powerapps.com/play/e/default-3255306b-4eff-41c9-89fb-3e24e65f48a1/a/f1e99b17-afcd-4d42-9af3-c4f9ee79469d?tenantId=3255306b-4eff-41c9-89fb-3e24e65f48a1",
     ):
         self.domain = domain
         self.username = username
@@ -26,14 +27,20 @@ class PowerApp:
         try:
             self.page.bring_to_front()
             self.page.goto(self.domain, wait_until="domcontentloaded")
-            self.page.frame_locator("iframe[class='player-app-frame']").locator("input[appmagic-control='idtextbox']").fill(self.username)
-            self.page.frame_locator("iframe[class='player-app-frame']").locator("input[appmagic-control='pwtextbox']").fill(self.password)
-            self.page.frame_locator("iframe[class='player-app-frame']").locator("button",has_text="Login").click()
+            self.page.frame_locator("iframe[class='player-app-frame']").locator(
+                "input[appmagic-control='idtextbox']"
+            ).fill(self.username)
+            self.page.frame_locator("iframe[class='player-app-frame']").locator(
+                "input[appmagic-control='pwtextbox']"
+            ).fill(self.password)
+            self.page.frame_locator("iframe[class='player-app-frame']").locator("button", has_text="Login").click()
             try:
-                self.page.frame_locator("iframe[class='player-app-frame']").locator("button",has_text="Login").wait_for(timeout=10000,state="detached")
+                self.page.frame_locator("iframe[class='player-app-frame']").locator(
+                    "button", has_text="Login"
+                ).wait_for(timeout=10000, state="detached")
                 return True
-            except TimeoutError:
-                raise PermissionError("PowerApp - PermissionError")    
+            except TimeoutError as e:
+                raise PermissionError("PowerApp - PermissionError") from e
         except TimeoutError:
             return self.login()
 
@@ -44,7 +51,7 @@ class PowerApp:
     def __exit__(self, exc_type, exc_value, traceback):
         if self.browser:
             self.browser.close()
-        
+
     def up(
         self,
         process_date: str,
@@ -53,30 +60,37 @@ class PowerApp:
     ) -> bool:
         try:
             self.page.bring_to_front()
-            for i in range(0,2):
-                dropdown = self.page.frame_locator("iframe[class='player-app-frame']").locator("div[aria-haspopup='listbox']").nth(i)
+            for i in range(0, 2):
+                dropdown = (
+                    self.page.frame_locator("iframe[class='player-app-frame']")
+                    .locator("div[aria-haspopup='listbox']")
+                    .nth(i)
+                )
                 while True:
                     if dropdown.get_attribute("aria-expanded") == "true":
-                        time.sleep(1/3)
+                        time.sleep(1 / 3)
                         break
                     dropdown.click()
-                    time.sleep(1/3)
-                time.sleep(1/3)
-                for j in range(self.page.frame_locator("iframe[class='player-app-frame']").locator("div[role='option']").count()):
-                    option = self.page.frame_locator("iframe[class='player-app-frame']").locator("div[role='option']").nth(j)
-                    if option.text_content() in [process_date,factory] and option.is_visible():
+                    time.sleep(1 / 3)
+                time.sleep(1 / 3)
+                for j in range(
+                    self.page.frame_locator("iframe[class='player-app-frame']").locator("div[role='option']").count()
+                ):
+                    option = (
+                        self.page.frame_locator("iframe[class='player-app-frame']").locator("div[role='option']").nth(j)
+                    )
+                    if option.text_content() in [process_date, factory] and option.is_visible():
                         option.click()
-                        time.sleep(1/3)
+                        time.sleep(1 / 3)
                         break
             self.page.frame_locator("iframe[class='player-app-frame']").locator("input[placeholder='案件検索']").clear()
-            time.sleep(1/3)
-            self.page.frame_locator("iframe[class='player-app-frame']").locator("input[placeholder='案件検索']").fill(build)
-            time.sleep(2/3)
+            time.sleep(1 / 3)
+            self.page.frame_locator("iframe[class='player-app-frame']").locator("input[placeholder='案件検索']").fill(
+                build
+            )
+            time.sleep(2 / 3)
             if self.page.frame_locator("iframe[class='player-app-frame']").locator("div[role='listitem']").count() != 1:
                 return False
             return False
         except TimeoutError:
-            return self.up(process_date,factory,build)
-
-
-
+            return self.up(process_date, factory, build)
