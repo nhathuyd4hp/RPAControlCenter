c20803e326cd307f777101bdde7f0ed437d68b16
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2026-01-05 13:59:43 +0700

    fix: path download ngoai hanwa

--- src/robot/SeikyuNgoaiHanwa/tasks.py
+++ src/robot/SeikyuNgoaiHanwa/tasks.py
@@ -164,7 +164,7 @@ def seikyu(
                     steps=[
                         re.compile("^(見積|見積書)$"),
                     ],
-                    file=".*.(xlsx|xlsm|xls)",
+                    file=re.compile(".*.(xlsx|xlsm|xls)"),
                 )
                 if not downloads or len(downloads) > 1:
                     quote_url.append(row["見積URL"])
