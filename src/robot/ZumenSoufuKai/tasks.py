6104fad0871adcc03c84f0c33b472e62b63be4de
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2026-01-12 14:23:08 +0700

    feat(ZumenSoufuKai): add task implementation and integrate into robot module

---
+++ src/robot/ZumenSoufuKai/tasks.py
@@ -0,0 +1,30 @@
+import shutil
+from celery import shared_task
+import subprocess
+from pathlib import Path
+
+@shared_task(bind=True,name="Zumen Soufu Kai")
+def ZumenSoufuKai(self):
+    exe_path = Path(__file__).resolve().parents[2] / "robot" / "ZumenSoufuKai" / "Zumen_Soufu_Kai_V2.8.exe"
+    cwd_path = exe_path.parent
+
+    log_dir = Path(__file__).resolve().parents[3] / "logs"
+    log_dir.mkdir(parents=True, exist_ok=True)
+    log_file = log_dir / f"{self.request.id}.log"
+
+    with open(log_file, "w", encoding="utf-8", errors="ignore") as f:
+        process = subprocess.Popen([str(exe_path)], cwd=str(cwd_path), stdout=f, stderr=subprocess.STDOUT, text=True)
+        process.wait()
+    # Clean
+    log_file = cwd_path / "Access_token_log"
+    try:
+        log_file.unlink()
+    except Exception:
+        pass
+
+    logs_folder = cwd_path / "Logs"
+    access_token_folder = cwd_path / "Access_token"
+    for path in (logs_folder, access_token_folder):
+        shutil.rmtree(path, ignore_errors=True)
+
+    
