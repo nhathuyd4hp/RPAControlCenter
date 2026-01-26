350aa1b19c99d49da95499e725ba0717929e6e33
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2026-01-26 10:45:47 +0700

    feat(upload): enhance get_asset to stream files with proper headers

--- src/robot/HajimeAnkenka/tasks.py
+++ src/robot/HajimeAnkenka/tasks.py
@@ -1,8 +1,10 @@
 from celery import shared_task
 from celery.app.task import Context, Task
 
+from src.core.inactive_task import InactiveTask
 
-@shared_task(bind=True, name="Hajime Ankenka")
+
+@shared_task(bind=True, name="Hajime Ankenka", base=InactiveTask)
 def HajimeAnkenka(self: Task):
     context: Context = self.request
     id = context.id
