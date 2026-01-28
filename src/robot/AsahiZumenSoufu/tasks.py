90161cf850571d655c07923ea59c3ff0a7522594
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2026-01-28 15:55:56 +0700

    feature(AsahiZumenSoufu): new Robot (#53)

---
+++ src/robot/AsahiZumenSoufu/tasks.py
@@ -0,0 +1,56 @@
+import io
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+from celery import shared_task
+from celery.app.task import Context, Task
+
+from src.core.config import settings
+from src.service import StorageService as minio
+
+
+@shared_task(bind=True, name="Asahi Zumen Soufu")
+def main(self: Task, file: io.BytesIO | str = "xlsx"):
+    # ----- Metadata -----#
+    context: Context = self.request
+    id = context.id
+    # ----- Download Asset -----#
+    file_name = os.path.basename(file) if (file, str) else file.name
+    save_path: Path = Path(__file__).resolve().parents[2] / "robot" / "AsahiZumenSoufu" / file_name
+    minio.fget_object(
+        bucket_name=settings.TEMP_BUCKET,
+        object_name=file,
+        file_path=str(save_path),
+    )
+    # ----- Rename -----#
+    new_path = save_path.with_name("Kizuku図面送付.xlsx")
+    save_path.replace(new_path)
+    # ----- Run Exe -----#
+    log_dir = Path(__file__).resolve().parents[3] / "logs"
+    log_dir.mkdir(parents=True, exist_ok=True)
+    log_file = log_dir / f"{id}.log"
+    exe_path = Path(__file__).resolve().parents[2] / "robot" / "AsahiZumenSoufu" / "Asahi_ZumenSoufuV1.2.exe"
+    with open(log_file, "w", encoding="utf-8", errors="ignore") as f:
+        process = subprocess.Popen(
+            [
+                sys.executable,
+                str(exe_path),
+            ],
+            cwd=str(exe_path.parent),
+            stdout=f,
+            stderr=subprocess.STDOUT,
+            text=True,
+            encoding="utf-8",
+        )
+        process.wait()
+    # ----- Upload Exe -----#
+    result = minio.fput_object(
+        bucket_name=settings.RESULT_BUCKET,
+        object_name=f"AsahiZumenSoufu/{id}/Kizuku.xlsx",
+        file_path=str(new_path),
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+    new_path.unlink(missing_ok=True)
+    return f"{settings.RESULT_BUCKET}/{result.object_name}"
